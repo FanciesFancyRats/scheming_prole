3917d594afda3bc5c181f9d96c90db779e508a05
FanciesFancyRats <magriffi1986@gmail.com>
2018-05-27 16:53:24 -0400

    log10 is what you are looking for

--- s4-6.scm
+++ s4-6.scm
@@ -17,6 +17,35 @@
 ;(define scientific
 ;  (lambda (x n
 
-(define x-pwr-10-iter
+(define x-to-n
   (lambda (x n)
-    
+	(cond
+	((< n 1) x)
+	(else (x-to-n (* x 10)(- n 1))))))
+(define x-to-n-neg
+  (lambda (x n)
+	(cond
+	((> n -1) x)
+	(else (x-to-n-neg (/ x 10)(+ n 1)))))) 
+(define sci-co-p
+  (lambda (x)
+	(cond
+	((not (= (modulo x 10) 0)) x)
+	(else (sci-co-p (/ x 10))))))
+(define sci-co-n
+  (lambda (x)
+	(cond
+	((not (= (modulo x 10) 0)) x)
+	(else (sci-co-n (* x 10))))))
+(define sci-co
+	(lambda (x)
+		(cond
+		((> x 0) (sci-co-p x))
+		((< x 1) (sci-co-n x))
+		(else 0))))
+(define scientific
+	(lambda (x n)
+		(cond
+		((> n 0) (x-to-n x n))
+		((< n 0) (x-to-n-neg x n))
+		(else x))))
