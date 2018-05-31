948af2cb1f7278adc2ec83a39ae7bf9c3e91f6ed
FanciesFancyRats <magriffi1986@gmail.com>
2018-05-31 06:25:31 -0500

    I don't think I actually did anything

--- s7.scm
+++ s7.scm
@@ -1,4 +1,6 @@
 (define (ger wd)
-  (let ((ana (if (vowel? (first wd)) (se 'an x 'is) (se 'a x 'is))))
+  (let ((ana (if (vowel? (first wd)) (se 'an wd 'is) (se 'a wd 'is))))
    (se (ana wd) (ana wd) (if (vowel? (first wd)) 'an 'a) wd))) 
 
+(define (vowel? letter)
+  (if (member? letter '(a e i o ui)) #t #f))
