778b9ff8f8c40e80e93871eab994fe7a305da787
FanciesFancyRats <magriffi1986@gmail.com>
2018-05-31 15:00:21 -0500

    Well this is embarassing...

--- s7.scm
+++ s7.scm
@@ -1,6 +1,13 @@
 (define (ger wd)
-  (let ((ana (if (vowel? (first wd)) (se 'an wd 'is) (se 'a wd 'is))))
-   (se (ana wd) (ana wd) (if (vowel? (first wd)) 'an 'a) wd))) 
+  (let ((ana (if (vowel? (first wd)) 'true 'false)))
+   (se (ana wd) (ana wd) (if (vowel? (first wd)) 'an 'a) wd)))
+;;
+;(define (vowel? letter)
+;  (if (member? letter '(a e i o ui)) #t #f))
 
-(define (vowel? letter)
-  (if (member? letter '(a e i o ui)) #t #f))
+(define (test x)
+  (let ((thing (sqrt x))
+	(thingb (* x x)))
+    (se thing thingb)))
+
+
