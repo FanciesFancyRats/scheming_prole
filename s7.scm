00f9f568e0d2a05b5e91a855c0709953d7d0108a
FanciesFancyRats <magriffi1986@gmail.com>
2018-06-01 13:14:16 -0500

    finally got it

--- s7.scm
+++ s7.scm
@@ -1,13 +1,30 @@
-(define (ger wd)
-  (let ((ana (if (vowel? (first wd)) 'true 'false)))
-   (se (ana wd) (ana wd) (if (vowel? (first wd)) 'an 'a) wd)))
-;;
-;(define (vowel? letter)
-;  (if (member? letter '(a e i o ui)) #t #f))
+(define (ger wrd)
+  (let ((ana (if (vowel? (first wrd)) 'an 'a)))
+    (se ana wrd 'is ana wrd 'is ana wrd))) 
+
+
+(define (vowel? letter)
+  (if (member? letter '(a e i o ui)) #t #f))
 
 (define (test x)
   (let ((thing (sqrt x))
 	(thingb (* x x)))
     (se thing thingb)))
 
+(define (piething)
+  (let ((pi '(3.14159))
+  	(pie '(lemon meringue)))
+  (se 'pi 'is pi 'but 'pie 'is pie)))
 
+(define (sup adj wrd)
+  (se (word adj 'est) wrd))
+
+(define (sum-sqr a b)
+  (let ((+ *)
+	(* +))
+    (* (+ a a) (+ b b))))
+(define (add2 n)
+  (+ n 2))
+
+(define (a2e lst)
+  (every add2 lst))
